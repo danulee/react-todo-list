b2c0e51ee3b6c8336d48b558ec7f8b4c1964714b
danulee <idanu9879@gmail.com>
2023-02-09 20:38:01 +0900

    immer, classnames 등 모듈 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { AppBar, Toolbar } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
 
+function MainPage() {
+  return (
+    <>
+      <h1>메인 페이지</h1>
+    </>
+  );
+}
+
+function Sub1Page() {
+  return (
+    <>
+      <h1>서브 페이지</h1>
+    </>
+  );
+}
 
 function App() {
   return (
   <>
-  <div>안녕</div>
+      <AppBar position="static">
+        <Toolbar>
+          <div className="flex-1"></div>
+          <span className="font-bold">HAPPY NOTE</span>
+          <div className="flex-1"></div>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/sub1" element={<Sub1Page />} />
+        <Route path="*" element={<Navigate to="/main" />} />
+      </Routes>
   </>
   );
 }
