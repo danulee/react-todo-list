ee25175a1e24857fd8e7a8d5649e62da60688055
danulee <idanu9879@gmail.com>
2023-02-14 20:09:54 +0900

    WritePage에서 할일 추가 구현

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import { AppBar, Toolbar} from "@mui/material";
-import { 
-  Routes, 
-  Route, 
-  Navigate, 
-  useLocation, 
+import { AppBar, Toolbar } from "@mui/material";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
   NavLink,
 } from "react-router-dom";
+
 import MainPage from "./pages/MainPage";
 import WritePage from "./pages/WritePage";
-import NoticeSnackbar from "./components/NoticeSnackbar";
+import { NoticeSnackbar } from "./components/NoticeSnackbar";
 
 export default function App() {
   const location = useLocation();
+
   return (
-  <>
+    <>
       <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
-          <span className="font-bold select-none">이름</span>
+          <span className="font-bold select-none">앱 이름</span>
           <div className="flex-1 flex justify-end">
-            {location.pathname !="/write" && (<NavLink to ="/write" className="select-none">글쓰기</NavLink>)}
-            {location.pathname =="/write" && (<NavLink to ="/main" className="select-none">메인</NavLink>)}
+            {location.pathname != "/write" && (
+              <NavLink to="/write" className="select-none">
+                글쓰기
+              </NavLink>
+            )}
+            {location.pathname == "/write" && (
+              <NavLink to="/main" className="select-none">
+                이전
+              </NavLink>
+            )}
           </div>
         </Toolbar>
       </AppBar>
+      <NoticeSnackbar />
       <Routes>
         <Route path="/main" element={<MainPage />} />
         <Route path="/write" element={<WritePage />} />
         <Route path="*" element={<Navigate to="/main" />} />
       </Routes>
-  </>
+    </>
   );
 }
