c4ab7f88e5a07b8a709942ef662e73a51530831f
danulee <idanu9879@gmail.com>
2023-02-16 19:30:34 +0900

    정렬 탭 형식 구현

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -4,7 +4,7 @@ import { useRecoilState } from "recoil";
 import { useTodosStatus, useTodoOptionDrawerStatus } from "../hooks";
 import TodoOptionDrawer from "../components/TodoOptionDrawer";
 import TodoListItem from "../components/TodoListItem";
-import { TodoList__filterCompletedIndexAtom } from "../atoms";
+import { TodoList__filterCompletedIndexAtom , TodoList__sortIndexAtom } from "../atoms";
 
 export default function TodoList() {
   const todosStatus = useTodosStatus();
@@ -13,6 +13,7 @@ export default function TodoList() {
   const [filterCompletedIndex, setFilterCompletedIndex] = useRecoilState(
     TodoList__filterCompletedIndexAtom
   );
+  const [sortIndex, setSortIndex] = useRecoilState(TodoList__sortIndexAtom);
 
   const getFliteredTodos = () => {
     if (filterCompletedIndex == 1) {
@@ -32,6 +33,7 @@ export default function TodoList() {
     <>
       <TodoOptionDrawer status={todoOptionDrawerStatus} />
 
+      {/* 필터링 탭 */}
       <Tabs
         variant="fullWidth"
         value={filterCompletedIndex}
@@ -65,7 +67,60 @@ export default function TodoList() {
           value={2}
         />
       </Tabs>
-
+     {/* 정렬 탭 */}
+      <Tabs
+        variant="scrollable"
+        value={sortIndex}
+        onChange={(event, newValue) => {
+          setSortIndex(newValue);
+        }}
+      >
+        <Tab
+          className="flex-grow !max-w-[none] px-4"
+          label={
+            <span className="flex items-baseline">
+              <i className="fa-regular fa-clock mr-2"></i>
+              <span className="mr-2 whitespace-nowrap">급해요</span>
+              <i className="fa-solid fa-sort-up relative top-[3px]"></i>
+            </span>
+          }
+          value={0}
+        />
+        <Tab
+          className="flex-grow !max-w-[none] px-4"
+          label={
+            <span className="flex items-baseline">
+              <i className="fa-regular fa-clock mr-2"></i>
+              <span className="mr-2 whitespace-nowrap">널널해요</span>
+              <i className="fa-solid fa-sort-down relative top-[-3px]"></i>
+            </span>
+          }
+          value={1}
+        />
+        <Tab
+          className="flex-grow !max-w-[none] px-4"
+          label={
+            <span className="flex items-baseline">
+              <i className="fa-solid fa-pen mr-2"></i>
+              <span className="mr-2 whitespace-nowrap">작성순</span>
+              <i className="fa-solid fa-sort-up relative top-[3px]"></i>
+            </span>
+          }
+          value={2}
+        />
+        <Tab
+          className="flex-grow !max-w-[none] px-4"
+          label={
+            <span className="flex items-baseline">
+              <i className="fa-solid fa-pen mr-2"></i>
+              <span className="mr-2 whitespace-nowrap">작성순</span>
+              <i className="fa-solid fa-sort-down relative top-[-3px]"></i>
+            </span>
+          }
+          value={3}
+        />
+      </Tabs>
+      {/* 메인 */}
       <div className="mt-4 px-4">
         <ul>
           {filteredTodos.map((todo, index) => (
